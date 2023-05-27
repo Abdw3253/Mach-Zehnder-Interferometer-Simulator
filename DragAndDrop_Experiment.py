219917bb11c326ebc047b1051bf66a6b9021c4b1
cto <cto@adve.live>
2023-05-27 23:21:54 +0300

    experiment

---
+++ DragAndDrop_Experiment.py
@@ -0,0 +1,36 @@
+from PyQt5.QtWidgets import QApplication, QHBoxLayout, QWidget, QPushButton
+from PyQt5.QtCore import Qt, QMimeData
+from PyQt5.QtGui import QDrag
+
+class DragButton(QPushButton):
+
+    def mouseMoveEvent(self, e):
+
+        if e.buttons() == Qt.LeftButton:
+            drag = QDrag(self)
+            mime = QMimeData()
+            drag.setMimeData(mime)
+            drag.exec_(Qt.MoveAction)
+
+class Window(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.setAcceptDrops(True)
+
+        self.blayout = QHBoxLayout()
+        for l in ['A', 'B', 'C', 'D']:
+            btn = DragButton(l)
+            self.blayout.addWidget(btn)
+
+        self.setLayout(self.blayout)
+
+    def dragEnterEvent(self, e):
+        e.accept()
+
+
+app = QApplication([])
+w = Window()
+w.show()
+
+app.exec_()
